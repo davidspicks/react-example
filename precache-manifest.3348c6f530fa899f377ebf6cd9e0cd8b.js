ef7e17066d4e87a2bc5040ff0abbb10e350dede4
davidspicks <davidspicks@live.com>
2020-02-13 19:34:30 -0800

    Updates

---
+++ precache-manifest.3348c6f530fa899f377ebf6cd9e0cd8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b26a51258aca106f27783d672d789e",
+    "url": "/davidspicks/react-example/index.html"
+  },
+  {
+    "revision": "e61d5971685d59f2f9c5",
+    "url": "/davidspicks/react-example/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e79ab761bb62797663e6",
+    "url": "/davidspicks/react-example/static/js/2.265ca040.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/davidspicks/react-example/static/js/2.265ca040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61d5971685d59f2f9c5",
+    "url": "/davidspicks/react-example/static/js/main.37b6c6bd.chunk.js"
+  },
+  {
+    "revision": "3bb164420916767e2c86",
+    "url": "/davidspicks/react-example/static/js/runtime-main.376c1a02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/davidspicks/react-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
